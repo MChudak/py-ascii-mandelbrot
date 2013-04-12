2eaad5f748fd0a3ab611a0ed5fadcad5f6716992
Maciej Chudak <macioosch@gmail.com>
2013-04-12 17:05:45 +0200

    after nearly 2 years I've rewritten this program a bit - it's not terrible

--- py-ascii-mandelbrot.py
+++ py-ascii-mandelbrot.py
@@ -1,52 +1,43 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from numpy import linspace
+from sys import argv
 
-f=open('py-ascii-mandelrender','w')
+ra = -2.0	# fractal is drawn from z0 = ra + ia*j to z1 = rb + ib*j
+rb =  1.0	# whole (?) set: ra = -2.0, rb= 1.0, ia = -1.0, ib= 1.0
+ia = -1.0
+ib =  1.0
 
-ra=-2.0		# fractal is drawn from z0=ra+ia*j to z1=rb+ib*j
-rb= 1.0		# whole (?) set: ra=-2.0, rb= 1.0, ia=-1.0, ib= 1.0
-ia=-1.0
-ib= 1.0
-		# maxx/maxy best 3/2, it's width/height [in ascii chars!]
-maxy=200.0
-maxx=maxy*2.0*(rb-ra)/(ib-ia)
+# maxx/maxy best 3/2, it's width/height [in ascii chars!]
+if len(argv) <= 1:
+    maxx = 60.0
+else:
+    maxx = float(argv[1])
+maxy = maxx*0.5*(ib - ia)/(rb - ra)
 
-maxiter=200	# maxiter defines precision of telling if a point\
-		# belongs to the set;
-		# it should be: d(maxiter)/d(min(|rb-ra|,|ib-ia|)) < 0,
-		# meaning: if you magnify, it's harder to tell the points apart
-		# also: higher value => slower, but better quality
+brdr = 5.0	# look for right brdr by guess :p
 
-brdr=5.0	# look for right brdr by guess :p
+maxiter = 200
+# maxiter defines precision of telling if a point
+# belongs to the set;
+# it should be: d(maxiter)/d(min(|rb - ra|,|ib - ia|)) < 0,
+# meaning: if you magnify, it's harder to tell the points apart
+# also: higher value => slower, but better quality
 
-pix='W'		# it's PIXel, HALf, and EMPty - program's brushes
-hal='x'
-emp='.'
+pix = 'W'	# it's PIXel, HALf, and EMPty - program's brushes
+hal = 'x'
+emp = '.'
 
-y=0
-while y<maxy:
-    line=''
-    
-    x=0
-    while x<maxx:
-        z0=ra+(x/maxx)*(rb-ra)+(ia+(y/maxy)*(ib-ia))*1j
-        z=z0
-        
-        i=0
-        while i<maxiter:
-            z=z*z+z0		# <- all magic is here!
-            i+=1
-            
-        if ((abs(z.real)*15<brdr)&(abs(z.imag)*15<brdr)):
-            line+=hal
+for imaginary in linspace(ia, ib, maxy):
+    line = ''
+    for real in linspace(ra, rb, maxx):
+        z0 = z = real + imaginary*1j
+        for i in range(maxiter):
+            z = z*z + z0	# <- all magic is here!
+        if (abs(z.real)*15<brdr) and (abs(z.imag)*15<brdr):
+            line += hal
         elif ((abs(z.real)<brdr)&(abs(z.imag)<brdr)):
-            line+=pix
+            line += pix
         else:
-            line+=emp
-            
-        x+=1
-
-    line+='\n'
-    f.write(line)
-
-    y+=1
+            line += emp
+    print(line)
 
